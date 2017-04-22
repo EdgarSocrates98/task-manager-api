a2dfaa3fa8f36d8d029428a2fa460de3343a091b
EdgarSocrates98 <edgarsocrates98@gmail.com>
2017-04-22 09:36:55 -0300

    Adding create action to the sessions controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -38,8 +38,8 @@ class Api::V1::UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
-
 end
