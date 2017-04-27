561f90bd87b1bf56c06903cdf603808c8234f51c
EdgarSocrates98 <edgarsocrates98@gmail.com>
2017-04-27 14:42:30 -0300

    Adding association with tasks for user model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   validates_uniqueness_of :auth_token
   before_create :generate_authentication_token!
 
+  has_many :tasks, dependent: :destroy
+
   def info
     "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
   end
@@ -14,6 +16,6 @@ class User < ApplicationRecord
   def generate_authentication_token!
     begin
       self.auth_token = Devise.friendly_token
-    end while User.exists?(auth_token: self.auth_token)
+    end while User.exists?(auth_token: auth_token)
   end
 end
